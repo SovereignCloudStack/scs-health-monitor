d9aa927e2cbf8a498675e67c7fcc09923fd2a389
Katharina Trentau <katharina.trentau@cloudandheat.com>
2024-03-06 13:28:12 +0100

    testing
    Signed-off-by: Katharina Trentau <katharina.trentau@cloudandheat.com>

---
+++ logging_test.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
