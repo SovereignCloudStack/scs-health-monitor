cebac11f775fc50f359d4ec50e10a52a9583d305
Katharina Trentau <katharina.trentau@cloudandheat.com>
2024-03-11 13:58:12 +0100

    created doc
    Signed-off-by: Katharina Trentau <katharina.trentau@cloudandheat.com>

--- loggerClassConfigYML.py
+++ loggerClassConfigYML.py
@@ -9,25 +9,11 @@ or print error, open a logger instance and print log messages
 try:
     with open('config.yml', 'r') as f:
         config = yaml.safe_load(f.read())
-        # # Ensure the 'version' key is present
         if 'version' not in config:
             config['version'] = 1
         logging.config.dictConfig(config)
 except Exception as e:
          print(f"Error loading logging configuration: {e}")
-
-# # Set up the handlers and formatters
-# handlers = config['handlers']
-# formatters = config['formatters']
-
-# for handler_config in handlers:
-#     handler_name = handler_config['name']
-#     handler_class = eval(handler_config['class'])
-#     handler = handler_class(*handler_config.get('args', []))
-#     handler.setLevel(handler_config['level'])
-#     handler.setFormatter(logging.Formatter(formatters[handler_config['formatter']]['format']))
-#     logging.getLogger().addHandler(handler)
-
 
 
 logger = logging.getLogger(__name__)
