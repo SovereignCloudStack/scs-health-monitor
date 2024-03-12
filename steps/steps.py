c7aae1495cdf0465e6b9a7f138950ef910cd1404
Piotr Bigos <piobig1@gmail.com>
2024-03-12 16:34:26 +0100

    updating logic, fixing assert
    Signed-off-by: Piotr Bigos <piobig1@gmail.com>

--- steps/steps.py
+++ steps/steps.py
@@ -2,7 +2,6 @@ from environment_setup import before_all, after_all
 import yaml
 from behave import given, when, then
 import openstack
-from typing import String
 
 def before_all(context):
     before_all(context)
@@ -28,7 +27,7 @@ class StepsDef:
         context.client = openstack.connect(cloud="gx")
 
     @when('A network with name {network_name} exists')
-    def when_i_connect_to_openstack(context, network_name: String):
+    def when_i_connect_to_openstack(context, network_name: str):
         network = context.client.network.find_network(name_or_id=network_name)
         assert network is not None, f"Network with {network_name} doesn't exists"
 
@@ -38,15 +37,14 @@ class StepsDef:
         assert networks, "Failed to list networks. No networks found."
 
     @then('I should be able to create a network with name {network_name}')
-    def then_i_should_be_able_to_create_a_network(context, network_name: String):
+    def then_i_should_be_able_to_create_a_network(context, network_name: str):
         network = context.client.network.find_network(name_or_id=network_name)
         assert network is None, f"Network with {network_name} already exists"
         context.client.network.create_network(name=network_name)
 
     @then('I should be able to delete a network with name {network_name}')
-    def then_i_should_be_able_to_delete_a_network(context, network_name):
+    def then_i_should_be_able_to_delete_a_network(context, network_name: str):
         network = context.client.network.find_network(name_or_id=network_name)
         assert network is not None, f"Network with {network_name} doesn't exists"
-        context.client.network.delete_network(network_name)
-        network = context.client.network.find_network(name_or_id=network_name)
-        assert network is None, f"Network called {network_name} wasn't deleted"
+        context.client.network.delete_network(network)
+        assert not context.client.network.find_network(name_or_id=network), f"Network called {network_name} already deleted"
