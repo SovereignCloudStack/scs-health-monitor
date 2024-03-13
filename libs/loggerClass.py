457550977f69b97579ad859ac9e0bc3f4ecf8420
Katharina Trentau <katharina.trentau@cloudandheat.com>
2024-03-13 10:09:04 +0100

    cleaned up
    Signed-off-by: Katharina Trentau <katharina.trentau@cloudandheat.com>

--- libs/loggerClass.py
+++ libs/loggerClass.py
@@ -1,36 +1,25 @@
 import logging
+
+
 class Logger:
-    def __init__(self, name='root', level=logging.DEBUG, log_file="logfile.log"):
-        #if not self.__shared_state:
-            self.logger = logging.getLogger(name)
-            self.logger.setLevel(level)
-            self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    def __init__(self, name="root", level=logging.DEBUG, log_file="logfile.log"):
+        # if not self.__shared_state:
+        self.instance = logging.getLogger(name)
+        self.instance.setLevel(level)
+        self.formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
 
-            console_handler = logging.StreamHandler()
-            console_handler.setLevel(level)
-            console_handler.setFormatter(self.formatter)
-            self.logger.addHandler(console_handler)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
+        console_handler.setFormatter(self.formatter)
+        self.instance.addHandler(console_handler)
 
-            # File handler (if log_file is provided)
-            if log_file:
-                file_handler = logging.FileHandler(log_file)
-                file_handler.setLevel(level)
-                file_handler.setFormatter(self.formatter)
-                self.logger.addHandler(file_handler)
-        # else:
-        #     self.__dict__ = self.__shared_state           
-    def getLogger(self):
-        return self.logger
+        if log_file:
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(level)
+            file_handler.setFormatter(self.formatter)
+            self.instance.addHandler(file_handler)
 
-# Example usage:
-# new_rootlogger = Logger()
-# logger_instance = new_rootlogger.getLogger()
-
-# logger_instance.info('This is an info message')
-# logger_instance.debug('This is a debug message')
-
-# new_otherlogger = Logger(name='other_logger', level=logging.DEBUG, log_file='otherlog.log')
-# logger_instance = new_otherlogger.getLogger()
-
-# logger_instance.info('This is an info message')
-# logger_instance.debug('This is a debug message')
+    # else:
+    #     self.__dict__ = self.__shared_state
--- openstack_inspector.py
+++ openstack_inspector.py
@@ -2,25 +2,19 @@ import openstack
 import yaml
 from libs.loggerClass import Logger
 
+
 class Inspector:
     def __init__(self, env_file_path="env.yaml"):
-        self.log = Logger(name='inspector_logger', log_file='logfile.log')
-        self.logger_instance = self.log.getLogger()
-        #self.logger_instance = self.log.logger # basically the same
+        self.log = Logger(name="inspector_logger", log_file="logfile.log")
+        self.logger_instance = self.log.instance
         self.env_file_path = env_file_path
         self.env = self.load_env_from_yaml()
         self.client = openstack.connect(cloud="gx")
-        
-
-    def exampleLog(self):
-        self.logger_instance.info('Logging from AnotherClass')
-        self.logger_instance.debug('Debug message from AnotherClass')
-
 
     def load_env_from_yaml(self):
-        with open(self.env_file_path, 'r') as file:
+        with open(self.env_file_path, "r") as file:
             env = yaml.safe_load(file)
-        #    self.logger_instance.debug("success:found environment variables")
+            self.logger_instance.debug("success:found environment variables")
         return env
 
     def check_network_existence(self, network_name):
@@ -28,26 +22,19 @@ class Inspector:
             networks = self.client.network.networks()
             for net in networks:
                 if net["name"] == network_name:
-                    #self.logger.info(f"Network found: {network_name}")
+                    self.logger_instance.info(f"Network found: {network_name}")
                     return True
-                else: continue
-            #TODO: log all of the networks to logfile
+                else:
+                    continue
         except Exception as e:
-            #self.logger.error(f"Error while checking network existence: {e}")
-            print(f"Error while checking network existence: {e}")
+            self.logger_instance.error(f"Error while checking network existence: {e}")
+
 
 # Example usage:
 if __name__ == "__main__":
 
     inspector = Inspector()
-    inspector.exampleLog()
     if inspector.check_network_existence("ext01"):
         print("Network exists!")
     else:
         print("Network does not exist.")
-
-
-# new_rootlogger = Logger()
-# logger_instance = new_rootlogger.getLogger()
-# logger_instance.info('This is an info message')
-# logger_instance.debug('This is a debug message')
