f5f6508b99400ceaba49b01e0fef775fd1d2500d
Erik Kostelanský <162309194+Erik-Kostelansky-dNation@users.noreply.github.com>
2024-04-09 13:54:38 +0200

    Spacecat 598 export prometheus metrics from ssh client (#37)
    * Add ssh client
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    
    * Add ssh scenarios
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    
    * Add config for terraform, fix scenario names
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    
    * Implement comments from review
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    
    * Add time recorder class, test functionality
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    
    * Add metrics for SSH, testing metrics push
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    
    ---------
    
    Signed-off-by: Erik-Kostelansky-dNation <erik.kostelansky@dnation.cloud>
    Signed-off-by: Piotr <piobig2871@gmail.com>
    Co-authored-by: Piotr <piobig2871@gmail.com>

--- libs/SshClient.py
+++ libs/SshClient.py
@@ -1,6 +1,22 @@
 import paramiko
+from prometheus_client import Counter, Histogram
+from libs.TimeRecorder import TimeRecorder
+from libs.PrometheusExporter import CommandTypes, LabelNames
+
+class SshClientMetricLabels:
+    STATUS_CODE='STATUS_CODE'
+    HOST='host'
+    ENDPOINT='endpoint'
+
+class SshClientResultStatusCodes:
+    SUCCESS='200'
+    FAILURE='400'
 
 class SshClient:
+    connection_count = Counter('ssh_connections_total', 'Total number of SSH connections', [SshClientMetricLabels.STATUS_CODE, SshClientMetricLabels.HOST, LabelNames.COMMAND_LABEL])
+    connectivity_test_count = Counter('ssh_connectivity_tests_total', 'Total number of SSH connectivity tests', [SshClientMetricLabels.STATUS_CODE, SshClientMetricLabels.HOST, SshClientMetricLabels.ENDPOINT, LabelNames.COMMAND_LABEL])
+    connect_duration = Histogram('ssh_connect_duration_seconds', 'Durations of SSH connections', [SshClientMetricLabels.STATUS_CODE, SshClientMetricLabels.HOST, LabelNames.COMMAND_LABEL])
+
     def __init__(self, host, username, key_path):
         self.host = host
         self.username = username
@@ -12,6 +28,7 @@ class SshClient:
     def log(self, level, message):
         if self.logger and level >= self.min_log_level:
             self.logger.log(level, message)
+
 
     def execute_command(self, command, ignore_error_output=False):
         try:
@@ -27,24 +44,48 @@ class SshClient:
             raise RuntimeError(f"Failed to execute command '{command}' on server {self.host}: {e}")
     
     def connect(self):
-        self.client.connect(self.host, username=self.username, pkey=self.private_key)
+
+        def on_success(duration):
+            self.connection_count.labels(SshClientResultStatusCodes.SUCCESS, self.host, CommandTypes.SSH).inc()
+            self.connect_duration.labels(SshClientResultStatusCodes.SUCCESS, self.host, CommandTypes.SSH).observe(duration)
+
+        def on_fail(duration, exception):
+            self.connection_count.labels(SshClientResultStatusCodes.FAILURE, self.host, CommandTypes.SSH).inc()
+            self.connect_duration.labels(SshClientResultStatusCodes.FAILURE, self.host, CommandTypes.SSH).observe(duration)
+
+        TimeRecorder.record_time(
+            lambda: self.client.connect(self.host, username=self.username, pkey=self.private_key),
+            on_success=on_success,
+            on_fail=on_fail
+        )
 
     def close_conn(self):
         self.client.close()
 
     def test_internet_connectivity(self, domain='google.com'):
-        try:
+        def test_connectivity():
+
             output = self.execute_command(f"ping -c 5 {domain}")
 
             # Check for packet loss in the output
             packet_loss_percentage = self.parse_packet_loss(output)
             if packet_loss_percentage == 100:
                 raise Exception(f"100% packet loss detected to {domain}")
-        
-            print(f"Internet connectivity test passed for server {self.host}, Output: {output}")
-        except Exception as e:
-            print(f"Failed to test internet connectivity for server {self.host}\nError --> {e}")
-            raise
+            
+        def on_success(duration):
+            self.connectivity_test_count.labels(SshClientResultStatusCodes.SUCCESS, self.host, domain, CommandTypes.SSH).inc()
+            print(f"Internet connectivity test passed for server {self.host}, Domain: {domain}")
+
+        def on_fail(duration, exception):
+            self.connectivity_test_count.labels(SshClientResultStatusCodes.FAILURE, self.host, domain, CommandTypes.SSH).inc()
+            print(f"Failed to test internet connectivity for server {self.host}, Domain: {domain}\nError: {exception}")
+
+        TimeRecorder.record_time(
+            test_connectivity,
+            on_success=on_success,
+            on_fail=on_fail
+    )
+
 
     def install_ping(self):
         command = "sudo apt-get update -y && sudo apt-get install -y iputils-ping"
---
+++ libs/TimeRecorder.py
@@ -0,0 +1,19 @@
+import time
+
+class TimeRecorder:
+    @staticmethod
+    def record_time(func, on_success=None, on_fail=None, *args, **kwargs):
+        start_time = time.time()
+        try:
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            duration = end_time - start_time
+            if on_success:
+                on_success(duration)
+            return result
+        except Exception as e:
+            end_time = time.time()
+            duration = end_time - start_time
+            if on_fail:
+                on_fail(duration, e)
+            raise
