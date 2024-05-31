74736febb3109c193ebc492a16e12272974879a0
Lubomir Dobrovodsky <dobrovlu@phactum.at>
2024-05-31 16:02:42 +0200

    Calculate Pi with Python
    Signed-off-by: Lubomir Dobrovodsky <dobrovlu@phactum.at>

--- features/steps/definitions-benchmark-pi.py
+++ features/steps/definitions-benchmark-pi.py
@@ -1,20 +1,16 @@
 from behave import given, when, then
 import time
-from libs.SshClient import SshClient
-
-from openstack.exceptions import DuplicateResource
 
 import tools
 
 class StepsDef:
-
     collector = tools.Collector()
 
-    @then("I should be able to benchmark 4000 digits of Pi on each VM")
+    @then("I should be able to calculate 4000 digits of Pi on each VM and measure time")
     def benchmark_pi_on_vms(context):
         results = []
         for ip in context.vm_ips[:context.jh_quantity]:
-            command = "{ TIMEFORMAT='%2U'; time echo 'scale=4000; 4*a(1)' | bc -l; } 2>&1"
+            command = 'python3 -c "from mpmath import mp; mp.dps = 4000; print(mp.pi)"'
             start_time = time.time()
             context.ssh_client.connect()
             try:
@@ -24,10 +20,3 @@ class StepsDef:
             end_time = time.time()
             duration = end_time - start_time
             results.append((ip, duration))
-        context.log_results(results)
-
-    def log_results(context, results):
-        with open('pi_benchmark_results.log', 'w') as log_file:
-            for result in results:
-                ip, duration = result
-                log_file.write(f"VM {ip}: {duration:.2f} s\n")
