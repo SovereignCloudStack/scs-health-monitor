13523ba8055bfbdf14beca923dca6a419f94414f
Katharina Trentau <katharina.trentau@cloudandheat.com>
2024-06-05 12:58:10 +0200

    Consolidating Clients
    Signed-off-by: Katharina Trentau <katharina.trentau@cloudandheat.com>

--- features/steps/definitions.py
+++ features/steps/definitions.py
@@ -493,13 +493,13 @@ class StepsDef:
         tools.verify_volumes_deleted(context.client, context.test_name)
         assert len(context.collector.volumes) == 0, f"Failed to delete volumes"
 
+###### from SshSteps:
     @given("I have deployed a VM with IP {vm_ip_address}")
     def initialize(context, vm_ip_address: str):
         context.vm_ip_address = vm_ip_address
     
     @given("I have a private key at {vm_private_ssh_key_path}")
     def check_private_key_exists(context, vm_private_ssh_key_path: str):
-        # Check if file exists
         context.vm_private_ssh_key_path = vm_private_ssh_key_path
         assert os.path.isfile(vm_private_ssh_key_path)
     
@@ -513,6 +513,13 @@ class StepsDef:
     def test_internet_connectivity(context):
         context.ssh_client.test_internet_connectivity()
 
+    @then("I should be able to collect all VM IPs")
+    def collect_ips(context):
+        domains=tools.collect_ips(context)
+        print(domains)
+
+    @then("be able to ping all IPs") 
+
     @then("be able to communicate with {domain}")
     def test_domain_connectivity(context, domain: str):
         result,assertline=context.ssh_client.test_internet_connectivity(domain)
@@ -523,35 +530,6 @@ class StepsDef:
         context.ssh_client.close_conn()
 
 ################
-
-    # @given("I have deployed a VM with IP {vm_ip_address}")
-    # def initialize(context, vm_ip_address: str):
-    #     context.vm_ip_address = vm_ip_address
-    
-    # @given("I have a private key at {vm_private_ssh_key_path}")
-    # def check_private_key_exists(context, vm_private_ssh_key_path: str):
-    #     # Check if file exists
-    #     context.vm_private_ssh_key_path = vm_private_ssh_key_path
-    #     assert os.path.isfile(vm_private_ssh_key_path)
-    
-    # @then("I should be able to SSH into the VM as user {username}")
-    # def test_ssh_connection(context, username):
-    #     ssh_client = SshClient(context.vm_ip_address, username, context.vm_private_ssh_key_path)
-    #     ssh_client.connect()
-    #     context.ssh_client = ssh_client
-
-    # @then("be able to communicate with the internet")
-    # def test_internet_connectivity(context):
-    #     context.ssh_client.test_internet_connectivity()
-
-    # @then("be able to communicate with {domain}")
-    # def test_domain_connectivity(context, domain: str):
-    #     result,assertline=context.ssh_client.test_internet_connectivity(domain)
-    #     assert result[1] == 0, assertline
-
-    # @then("close the connection")
-    # def close_connection(context):
-    #     context.ssh_client.close_conn()
 
 
         #  if __name__ == "__main__":
