032fa5cc2f9eae03afb2d85c2605c7e6052f769f
Katharina Trentau <katharina.trentau@cloudandheat.com>
2024-06-20 14:34:34 +0200

    create vms in tools
    Signed-off-by: Katharina Trentau <katharina.trentau@cloudandheat.com>

--- features/steps/definitions.py
+++ features/steps/definitions.py
@@ -133,10 +133,11 @@ class StepsDef:
     @then("I should be able to create {network_quantity:d} networks")
     def create_network(context, network_quantity: int):
         for num in range(1, network_quantity + 1):
-            network = context.client.network.create_network(name=f"{context.test_name}-network-{num}")
+            #network = context.client.network.create_network(name=f"{context.test_name}-network-{num}")
+            network = tools.create_network(context.client, name=f"{context.test_name}-network-{num}")
             context.collector.networks.append(network.id)
-            assert not context.client.network.find_network(
-                name_or_id=network), f"Network called {network} created"
+            # assert not context.client.network.find_network(
+            #     name_or_id=network), f"Network called {network} created"
         assert len(context.collector.networks) == network_quantity, \
             f"Failed to create the desired amount of networks"
 
@@ -410,25 +411,28 @@ class StepsDef:
         security_groups = [{"name": "default"}, {"name": "ping-sg"}]
         for network in context.client.network.networks():
             if context.test_name in network.name:
-                for num in range(1, vms_quantity + 1):
-                    vm_name = f"{context.test_name}-vm-{num}"
-                  #  vm_name = f"{context.test_name}-vm-{''.join(random.choices(string.ascii_letters + string.digits, k=10))}"
-                    image = context.client.compute.find_image(name_or_id=context.vm_image)
-                    assert image, f"Image with name {context.vm_image} doesn't exist"
-                    flavor = context.client.compute.find_flavor(name_or_id=context.flavor_name)
-                    assert flavor, f"Flavor with name {context.flavor_name} doesn't exist"
-                    assert network, f"Network with name {network.name} doesn't exist"
-                    try:
-                        server = context.client.compute.create_server(
-                            name=vm_name,
-                            image_id=image.id,
-                            flavor_id=flavor.id,
-                            networks=[{"uuid": network.id}],
-                            security_groups=security_groups
-                        )
-                        context.client.compute.wait_for_server(server)
-                    except DuplicateResource as e:
-                        assert e, "Server already created!"
+                # for num in range(1, vms_quantity + 1):
+                #     vm_name = f"{context.test_name}-vm-{num}"
+                #   #  vm_name = f"{context.test_name}-vm-{''.join(random.choices(string.ascii_letters + string.digits, k=10))}"
+                #     image = context.client.compute.find_image(name_or_id=context.vm_image)
+                #     assert image, f"Image with name {context.vm_image} doesn't exist"
+                #     flavor = context.client.compute.find_flavor(name_or_id=context.flavor_name)
+                #     assert flavor, f"Flavor with name {context.flavor_name} doesn't exist"
+                #     assert network, f"Network with name {network.name} doesn't exist"
+                #     try:
+                #         server = context.client.compute.create_server(
+                #             name=vm_name,
+                #             image_id=image.id,
+                #             flavor_id=flavor.id,
+                #             networks=[{"uuid": network.id}],
+                #             security_groups=security_groups
+                #         )
+                #         context.client.compute.wait_for_server(server)
+                #     except DuplicateResource as e:
+                #         assert e, "Server already created!"
+                for num in range(1, int(vms_quantity) + 1):
+                    vm_name = f"{context.test_name}-vm-{''.join(random.choices(string.ascii_letters + string.digits, k=10))}"
+                    tools.create_vm(context.client, vm_name, context.vm_image, context.flavor_name, network.id, security_groups=security_groups)
                     time.sleep(5)
                     created_server = context.client.compute.find_server(name_or_id=vm_name)
                     context.collector.virtual_machines.append(created_server.id)
