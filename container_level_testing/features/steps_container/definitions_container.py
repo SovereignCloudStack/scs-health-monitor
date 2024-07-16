878569e9127e3851e847b917e756efd3cd3e0e4c
Piotr Bigos <piobig1@gmail.com>
2024-07-16 11:20:29 +0200

    First iteration of tests
    Signed-off-by: Piotr Bigos <piobig1@gmail.com>

--- container_level_testing/features/steps_container/definitions_container.py
+++ container_level_testing/features/steps_container/definitions_container.py
@@ -1,52 +1,111 @@
-# import kubernetes.client
-# from kubernetes.client.rest import ApiException
-# from pprint import pprint
-#
-#
-# configuration = kubernetes.client.Configuration()
-#
-# configuration.api_key['authorization'] = 'YOUR_API_KEY'
-#
-# # Defining host is optional and default to http://localhost
-# configuration.host = env.get("")
-#
-# # Defining host is optional and default to http://localhost
-# configuration.host = "http://localhost"
-# # Enter a context with an instance of the API kubernetes.client
-# with kubernetes.client.ApiClient(configuration) as api_client:
-#     # Create an instance of the API class
-#     api_instance = kubernetes.client.WellKnownApi(api_client)
-#
-#     try:
-#         api_response = api_instance.get_service_account_issuer_open_id_configuration()
-#         pprint(api_response)
-#     except ApiException as e:
-#         print("Exception when calling WellKnownApi->get_service_account_issuer_open_id_configuration: %s\n" % e)
-#
-
+from behave import given, when, then
 from kubernetes import client, config
+import requests
+import time
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+class KubernetesTestSteps:
+    def __init__(self):
+        config.load_kube_config()
+        self.v1 = client.CoreV1Api()
+        self.response = None
+        self.ping_response = None
 
-from kubernetes import client, config, watch
+    @given('a Kubernetes cluster')
+    def step_given_kubernetes_cluster(self, context):
+        # Ensure we can connect to the cluster
+        self.v1.list_node()
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
+    @when('I create a container named "{container_name}"')
+    def step_when_create_container(self, context, container_name):
+        pod = client.V1Pod(
+            metadata=client.V1ObjectMeta(name=container_name),
+            spec=client.V1PodSpec(
+                containers=[client.V1Container(
+                    name=container_name,
+                    image="nginx",
+                    ports=[client.V1ContainerPort(container_port=80)]
+                )]
+            )
+        )
+        self.v1.create_namespaced_pod(namespace="default", body=pod)
+        time.sleep(10)  # Wait for the pod to be created
 
-v1 = client.CoreV1Api()
-count = 10
-w = watch.Watch()
-for event in w.stream(v1.list_namespace, _request_timeout=60):
-    print("Event: %s %s" % (event['type'], event['object'].metadata.name))
-    count -= 1
-    if not count:
-        w.stop()
+    @then('the container "{container_name}" should be running')
+    def step_then_container_running(self, context, container_name):
+        pod = self.v1.read_namespaced_pod(name=container_name, namespace="default")
+        assert pod.status.phase == "Running"
 
-print("Ended.")
+    @when('I send an HTTP request to "{container_name}"')
+    def step_when_send_http_request(self, context, container_name):
+        pod = self.v1.read_namespaced_pod(name=container_name, namespace="default")
+        ip = pod.status.pod_ip
+        self.response = requests.get(f"http://{ip}")
+
+    @then('the response status code should be 200')
+    def step_then_response_status_code(self, context):
+        assert self.response.status_code == 200
+
+    @when('"{src_container}" pings "{dst_container}"')
+    def step_when_ping(self, context, src_container, dst_container):
+        src_pod = self.v1.read_namespaced_pod(name=src_container, namespace="default")
+        dst_pod = self.v1.read_namespaced_pod(name=dst_container, namespace="default")
+        ip = dst_pod.status.pod_ip
+
+        exec_command = [
+            '/bin/sh',
+            '-c',
+            f'ping -c 1 {ip}'
+        ]
+        response = self.v1.connect_get_namespaced_pod_exec(
+            name=src_container,
+            namespace="default",
+            command=exec_command,
+            stderr=True,
+            stdin=False,
+            stdout=True,
+            tty=False
+        )
+        self.ping_response = response
+
+    @then('the ping should be successful')
+    def step_then_ping_successful(self, context):
+        assert "1 packets transmitted, 1 received" in self.ping_response
+
+
+k8s_steps = KubernetesTestSteps()
+
+
+@given('a Kubernetes cluster')
+def step_given_kubernetes_cluster(context):
+    k8s_steps.step_given_kubernetes_cluster(context)
+
+
+@when('I create a container named "{container_name}"')
+def step_when_create_container(context, container_name):
+    k8s_steps.step_when_create_container(context, container_name)
+
+
+@then('the container "{container_name}" should be running')
+def step_then_container_running(context, container_name):
+    k8s_steps.step_then_container_running(context, container_name)
+
+
+@when('I send an HTTP request to "{container_name}"')
+def step_when_send_http_request(context, container_name):
+    k8s_steps.step_when_send_http_request(context, container_name)
+
+
+@then('the response status code should be 200')
+def step_then_response_status_code(context):
+    k8s_steps.step_then_response_status_code(context)
+
+
+@when('"{src_container}" pings "{dst_container}"')
+def step_when_ping(context, src_container, dst_container):
+    k8s_steps.step_when_ping(context, src_container, dst_container)
+
+
+@then('the ping should be successful')
+def step_then_ping_successful(context):
+    k8s_steps.step_then_ping_successful(context)
