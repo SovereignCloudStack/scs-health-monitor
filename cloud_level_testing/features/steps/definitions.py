c17fc0c73cfe52191cdfbd639330c8dd982e5895
Piotr <piobig2871@gmail.com>
2024-08-14 11:31:37 +0200

    uncommenting connection test
    Signed-off-by: Piotr <piobig2871@gmail.com>

--- cloud_level_testing/features/steps/definitions.py
+++ cloud_level_testing/features/steps/definitions.py
@@ -18,12 +18,11 @@ class StepsDef:
 
     @given("I connect to OpenStack")
     def given_i_connect_to_openstack(context):
-        # cloud_name = context.env.get("CLOUD_NAME")
-        # context.test_name = context.env.get("TESTS_NAME_IDENTIFICATION")
-        # context.vm_image = context.env.get("VM_IMAGE")
-        # context.flavor_name = context.env.get("FLAVOR_NAME")
-        # context.client = openstack.connect(cloud=cloud_name)
-        pass
+        cloud_name = context.env.get("CLOUD_NAME")
+        context.test_name = context.env.get("TESTS_NAME_IDENTIFICATION")
+        context.vm_image = context.env.get("VM_IMAGE")
+        context.flavor_name = context.env.get("FLAVOR_NAME")
+        context.client = openstack.connect(cloud=cloud_name)
 
     @when("A router with name {router_name} exists")
     def router_with_name_exists(context, router_name: str):
